4b0cc799988ed2dd11c1c40b033aff24bf6f7bbf
Caio Navarro <caionavarro.n@hotmail.com>
2018-12-19 21:19:33 +1300

    Add Months to main application

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
+import Month from './components/Month';
 
 class App extends Component {
+  constructor () {
+    super();
+
+    this.months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            The year.
+            One calendar
           </p>
         </header>
+        <div className='Year'>
+          { this.months.map((m, i) => 
+              <Month name={m} index={i} year={2018} />
+            )
+          }
+        </div>
       </div>
     );
   }
