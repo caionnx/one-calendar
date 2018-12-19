5ceb18eabcb068630cd07f05b1a44c48a06ada9d
Caio Navarro <caionavarro.n@hotmail.com>
2018-12-19 21:17:59 +1300

    Add component for WeekHead

---
+++ src/components/WeekHead.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WeekHead = () => (
+  <tr className='WeekHead'>
+    <th>Su</th>
+    <th>M</th>
+    <th>Tu</th>
+    <th>W</th>
+    <th>Th</th>
+    <th>F</th>
+    <th>Sa</th>
+  </tr>
+)
+
+export default WeekHead;
