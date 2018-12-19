66fc3f62ed79b32ca7ec6ff32c552b2353b445a6
Caio Navarro <caionavarro.n@hotmail.com>
2018-12-20 03:42:03 +1300

    Fix unity test and minor change in index.html

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 import App from './App';
+
+const mockStore = configureMockStore();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = mockStore([]);
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+    , div);
   ReactDOM.unmountComponentAtNode(div);
 });
